742259d5d34ad04a83b2222200e72e455effc6bc
SMSC-PMacmini <smscmacmini2@macmini2.local>
2019-01-17 12:13:10 +0530

    sampleJenkin

---
+++ SampleJenkin/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  SampleJenkin
+//
+//  Created by SMSC-PMacmini on 17/01/19.
+//  Copyright © 2019 phanindhra B V. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        print("samplejenkin")
+        
+    }
+
+
+}
+
